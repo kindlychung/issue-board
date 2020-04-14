e218c1116dd1fffa79a8f0a062ff7f5ffcc2ef3e
Leopold Luley <git@leopoldluley.de>
2020-04-14 13:54:23 +0200

    Setup druid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use druid::{AppLauncher, WindowDesc, LocalizedString, Data, Lens, Widget};
+
+#[derive(Clone, Data, Lens)]
+struct IssueBoard {}
+
+impl IssueBoard {
+    pub fn new() -> Self {
+        IssueBoard {}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let title = LocalizedString::new("Issue Board");
+    let window = WindowDesc::new(build_ui)
+        .title(title);
+
+    let state = IssueBoard::new();
+
+    AppLauncher::with_window(window)
+        .launch(state)
+        .expect("Failed to launch Issue Board");
+}
+
+fn build_ui() -> impl Widget<IssueBoard> {
+    druid::widget::Label::new("Test")
 }
