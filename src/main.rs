8734864a38d35c97d5b39e98ca734d82b95d618f
Leopold Luley <git@leopoldluley.de>
2020-04-16 22:20:58 +0200

    Add basic pagination

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,29 @@ struct Issue {
     author: Arc<str>,
 }
 
+struct QueryResult {
+    pub issues: Vec<Issue>,
+    pub next_page: GithubPage,
+}
+
 trait Backend {
-    fn query(&self, query: Query) -> Result<Vec<Issue>>;
+    fn query(&self, query: Query) -> Result<QueryResult>;
 }
 
 struct Github {
     the_query: Template,
 }
 
+#[derive(Debug, Clone, Default)]
+struct GithubPage {
+    end_cursor: Option<String>,
+}
+
 #[derive(Debug, Clone, Copy)]
 struct Query<'a> {
     owner: &'a str,
     repo: &'a str,
+    page: &'a GithubPage,
 }
 
 impl Github {
@@ -43,18 +54,19 @@ impl Github {
         let the_query = Template::new(include_str!("query.graphql"));
         Github { the_query }
     }
-
-    fn the_query(&self, owner: &str, repo: &str) -> String {
-        let mut args = std::collections::HashMap::new();
-        args.insert("owner", owner);
-        args.insert("repo", repo);
-        self.the_query.render(&args)
-    }
 }
 
 impl Backend for Github {
-    fn query(&self, query: Query) -> Result<Vec<Issue>> {
-        let query = self.the_query(query.owner, query.repo);
+    fn query(&self, query: Query) -> Result<QueryResult> {
+        let mut args = std::collections::HashMap::new();
+        args.insert("owner", query.owner);
+        args.insert("repo", query.repo);
+        let page = match &query.page.end_cursor {
+            Some(cursor) => format!("\"{}\"", cursor),
+            None => "null".to_owned(),
+        };
+        args.insert("page", &page);
+        let query = self.the_query.render(&args);
 
         let response = ureq::post(GITHUB_GRAPHQL_ENDPOINT)
             .auth_kind("bearer", include_str!("../github_token"))
@@ -83,20 +95,33 @@ impl Backend for Github {
                 .into();
             issues.push(Issue { title, author });
         }
-        Ok(issues)
-    }
-}
 
-impl<'a> Query<'a> {
-    pub fn new(owner: &'a str, repo: &'a str) -> Self {
-        Query { owner, repo }
+        let page_info = response
+            .pointer("/data/repository/issues/pageInfo")
+            .ok_or_else(|| {
+                anyhow!(
+                    "Response did not contain page info:\n{}",
+                    serde_json::to_string(&response).unwrap_or("Invalid JSON response".into())
+                )
+            })?;
+
+        let next_page = GithubPage {
+            end_cursor: Some(
+                page_info["endCursor"]
+                    .as_str()
+                    .ok_or(anyhow!("Page info had no endCursor"))?
+                    .into(),
+            ),
+        };
+
+        Ok(QueryResult { issues, next_page })
     }
 }
 
 impl IssueBoard {
     pub fn new() -> Self {
         IssueBoard {
-            columns: Arc::new(vec![IssueColumn::new(), IssueColumn::new()]),
+            columns: Default::default(),
         }
     }
 }
@@ -104,7 +129,7 @@ impl IssueBoard {
 impl IssueColumn {
     pub fn new() -> Self {
         IssueColumn {
-            issues: Arc::new(Vec::new()),
+            issues: Default::default(),
         }
     }
 }
@@ -118,17 +143,14 @@ fn main() {
 
     let mut board = IssueBoard::new();
 
-    {
-        let query = Query::new(owner, repo);
-        let issues = backend.query(query).expect("Failed to query Github");
-        let column = &mut Arc::make_mut(&mut board.columns)[0];
-        Arc::make_mut(&mut column.issues).extend(issues.into_iter());
-    }
-    {
-        let query = Query::new(owner, repo);
-        let issues = backend.query(query).expect("Failed to query Github");
-        let column = &mut Arc::make_mut(&mut board.columns)[1];
-        Arc::make_mut(&mut column.issues).extend(issues.into_iter());
+    let mut next_page = GithubPage::default();
+    for _ in 0..3 {
+        let query = Query { owner, repo, page: &next_page };
+        let result = backend.query(query).expect("Failed to query Github");
+        next_page = result.next_page;
+        let mut column = IssueColumn::new();
+        Arc::make_mut(&mut column.issues).extend(result.issues.into_iter());
+        Arc::make_mut(&mut board.columns).push(column);
     }
 
     AppLauncher::with_window(window)
@@ -149,7 +171,7 @@ impl IssueColumn {
         Scroll::new(
             List::rows(Issue::widget)
                 .padding(10.0)
-                .fix_width(300.0)
+                .fix_width(330.0)
                 .lens(Self::issues),
         )
         .vertical()
